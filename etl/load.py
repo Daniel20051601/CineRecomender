6e7c1a833793abfce5dbd80eee0505186a98b38a
Danielvi <danielvilopez@gmail.com>
2026-05-24 11:59:30 -0400

    ✨ Add load_new_movies function to insert movies into the database

--- etl/load.py
+++ etl/load.py
@@ -1,4 +1,5 @@
 from database.connection import get_engine
+from sqlalchemy import text
 
 engine = get_engine()
 
@@ -19,3 +20,46 @@ def load_categories(df):
         if_exists="replace",
         index=False
     )
+    
+def load_new_movies(data):
+    try:
+        with engine.begin() as conn:
+            for _, row in data.iterrows():
+
+                query = text("""
+                    INSERT INTO movies (
+                        id,
+                        title,
+                        backdrop_path,
+                        genre_ids,
+                        original_language,
+                        overview, 
+                        popularity,
+                        poster_path, 
+                        release_date, 
+                        vote_average,
+                        vote_count
+                    )
+                    VALUES (
+                        :id,
+                        :title,
+                        :backdrop_path,
+                        :genre_ids,
+                        :original_language,
+                        :overview,
+                        :popularity,
+                        :poster_path,
+                        :release_date,
+                        :vote_average,
+                        :vote_count
+                    )
+                    ON CONFLICT (id)
+                    DO NOTHING
+                """)
+
+                conn.execute(query, row.to_dict())
+        return True
+    
+    except Exception as e:
+        print('Error: ', e)
+        return False
